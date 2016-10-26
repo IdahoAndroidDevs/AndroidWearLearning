7a9aefbe630533b43d4e5c6865bcffebdcf45810
Joe Rowley (jrowley) <joseph.rowley@mlb.com>
2016-10-26 11:59:59 -1000

    Voice Reply notifications in place

--- mobile/src/main/java/rowley/androidwearlearning/NotificationConfirmationActivity.java
+++ mobile/src/main/java/rowley/androidwearlearning/NotificationConfirmationActivity.java
@@ -1,8 +1,11 @@
 package rowley.androidwearlearning;
 
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.widget.TextView;
 
 public class NotificationConfirmationActivity extends AppCompatActivity {
 
@@ -11,5 +14,26 @@ public class NotificationConfirmationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_confirmation);
         NotificationManagerCompat.from(this).cancelAll();
+
+        checkForVoiceResponse();
+    }
+
+    private void checkForVoiceResponse() {
+        CharSequence response = getMessageText();
+
+        if (!TextUtils.isEmpty(response)) {
+            ((TextView) findViewById(R.id.confirmation_text_view))
+                    .setText(String.format(getString(R.string.voice_reply_response_formatted), response));
+        }
+    }
+
+    private String getMessageText() {
+        String result = "";
+        Bundle remoteInput = RemoteInput.getResultsFromIntent(getIntent());
+        if (remoteInput != null) {
+            result = remoteInput.getCharSequence(NotificationsActivity.EXTRA_VOICE_REPLY).toString();
+        }
+
+        return result;
     }
 }
--- mobile/src/main/java/rowley/androidwearlearning/NotificationsActivity.java
+++ mobile/src/main/java/rowley/androidwearlearning/NotificationsActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,8 +17,11 @@ import butterknife.Unbinder;
 
 public class NotificationsActivity extends AppCompatActivity {
 
-    final int BASIC_NOTIFICATION_ID = 001;
-    final int CUSTOM_NOTIFICATION_ID = 002;
+    private final int BASIC_NOTIFICATION_ID = 001;
+    private final int CUSTOM_NOTIFICATION_ID = 002;
+    private final int VOICE_REPLY_NOTIFICATION_ID = 003;
+
+    public static final String EXTRA_VOICE_REPLY = "extraVoiceReply";
 
     private Unbinder unbinder;
 
@@ -50,6 +54,13 @@ public class NotificationsActivity extends AppCompatActivity {
 
         notificationInput = (EditText) findViewById(R.id.notification_input);
 
+        findViewById(R.id.voice_reply_notification_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendVoiceReplyNotification();
+            }
+        });
+
         NotificationManagerCompat.from(this).cancelAll();
     }
 
@@ -65,6 +76,12 @@ public class NotificationsActivity extends AppCompatActivity {
     void sendBasicNotification() {
         displayBasicNotification(getString(R.string.notification_title),
                 getString(R.string.notification_message), BASIC_NOTIFICATION_ID);
+    }
+
+//    @OnClick(R.id.custom_notification_button)
+    void sendCustomNotification() {
+        displayBasicNotification(getString(R.string.custom_notification_title),
+                notificationInput.getText().toString(), CUSTOM_NOTIFICATION_ID);
     }
 
     private void displayBasicNotification(String title, String message, int notificationId) {
@@ -87,10 +104,31 @@ public class NotificationsActivity extends AppCompatActivity {
         notifMan.notify(notificationId, notification);
     }
 
-//    @OnClick(R.id.custom_notification_button)
-    void sendCustomNotification() {
-        displayBasicNotification(getString(R.string.custom_notification_title),
-                notificationInput.getText().toString(), CUSTOM_NOTIFICATION_ID);
+//    @OnClick(R.id.voice_reply_notification_button)
+    void sendVoiceReplyNotification() {
+        Intent intent = new Intent(this, NotificationConfirmationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        RemoteInput remoteInput = new RemoteInput.Builder(EXTRA_VOICE_REPLY)
+                .setLabel(getString(R.string.voice_reply_instructions))
+                .setChoices(getResources().getStringArray(R.array.notification_voice_options)).build();
+
+        NotificationCompat.Action watchAction = new NotificationCompat.Action.Builder(
+                R.mipmap.ic_launcher, getString(R.string.notification_title), pendingIntent)
+                .addRemoteInput(remoteInput)
+                .build();
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentText(getString(R.string.voice_reply_notification_message))
+                .setContentTitle(getString(R.string.voice_reply_notification_title))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .extend(new NotificationCompat.WearableExtender().addAction(watchAction))
+                .setContentIntent(pendingIntent)
+                .setVibrate(new long[] {250, 250})
+                .build();
+
+        NotificationManagerCompat notifMan = NotificationManagerCompat.from(this);
+        notifMan.notify(VOICE_REPLY_NOTIFICATION_ID, notification);
     }
 
 }
